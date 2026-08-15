7d24fa7777066a89a07ebe8e3d6577a4fe37ecbd
Karina <ROCKYAJ2004@example.com>
2026-08-15 03:23:53 -0400

    add unquiie order numbers

--- server/models/orders.js
+++ server/models/orders.js
@@ -1,21 +1,40 @@
-const { default: mongoose } = require('mongoose');
-let moogoose = require('mongoose');
+const mongoose = require('mongoose');
 
-let ordersModel = mongoose.Schema({
-    customerName: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now },
-    status: { type: String, enum: ['Pending', 'Served', 'Cancelled'], default: 'Pending' }
-},
-{
+const ordersModel = new mongoose.Schema(
+  {
+    orderNumber: {
+      type: String,
+      unique: true,
+      default: () => `ORD-${Date.now()}-${Math.floor(Math.random() * 1000)}`
+    },
+
+    customerName: {
+      type: String,
+      required: true
+    },
+
+    createdAt: {
+      type: Date,
+      default: Date.now
+    },
+
+    status: {
+      type: String,
+      enum: ['Pending', 'Served', 'Cancelled'],
+      default: 'Pending'
+    }
+  },
+  {
     collection: 'orders'
-});
+  }
+);
 
 ordersModel.set('toJSON', {
-    virtuals: true,
-    versionKey: false,
-    transform: function (doc, ret) {
-        delete ret._id
-    }
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret) {
+    delete ret._id;
+  }
 });
 
-module.exports = mongoose.model("Orders", ordersModel);
+module.exports = mongoose.model('Orders', ordersModel);
